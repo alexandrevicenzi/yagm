ef3029dba56456b9cd8832b2a6667ec94d689c3d
lummie <lummie.co.uk@gmail.com>
2017-10-21 01:48:04 +0100

    An optimised route list As you were iterating the map of routes, I have converted this to an array and track the requests to each route. This is used to occasionally sort the array so the most common routes come first. How often can be controlled by RouteOptimizeRequestCount. Added a test.

--- yagm_test.go
+++ yagm_test.go
@@ -95,10 +95,76 @@ func TestRequestCycle(t *testing.T) {
 func TestNotFound(t *testing.T) {
 	mux := New()
 	request, _ := http.NewRequest("GET", "/foobar", nil)
-	respose := &Response{}
-	mux.ServeHTTP(respose, request)
+	response := &Response{}
+	mux.ServeHTTP(response, request)
 
-	if respose.Status != 404 {
+	if response.Status != 404 {
 		t.Fatal("Status should be 404.")
 	}
+}
+
+
+func TestRouteOptimization(t *testing.T){
+	mux := New()
+	handler := func(w http.ResponseWriter, r *http.Request) {
+	}
+	mux.HandleFunc("^/A[a-zA-Z]+$", handler)
+	mux.HandleFunc("^/B[a-zA-Z]+$", handler)
+
+	if mux.routes[0].pattern != "^/A[a-zA-Z]+$" {
+		t.Fatal("Expected first item in the array to be route A")
+	}
+	if mux.routes[0].hitCount != 0 {
+		t.Fatal("Expected route /A* to have a hit count of zero ")
+	}
+	if mux.routes[1].hitCount != 0 {
+		t.Fatal("Expected route /B* to have a hit count of zero ")
+	}
+
+	requestA, _ := http.NewRequest("GET", "/Aardvark", nil)
+	response := &Response{}
+	for i:=0; i<100; i++ {
+		mux.ServeHTTP(response, requestA)
+	}
+	if mux.routes[0].pattern != "^/A[a-zA-Z]+$" {
+		t.Fatal("Expected first item in the array to be route A")
+	}
+	if mux.routes[0].hitCount != 100 {
+		t.Fatal("Expected route /A* to have a hit count of 100 ", mux.routes[0].hitCount)
+	}
+	if mux.routes[1].hitCount != 0 {
+		t.Fatal("Expected route /B* to have a hit count of zero ", mux.routes[1].hitCount)
+	}
+
+
+	requestB, _ := http.NewRequest("GET", "/BananasBananas", nil)
+	for i:=0; i<200; i++ {
+		mux.ServeHTTP(response, requestB)
+	}
+
+	if mux.routes[0].pattern != "^/A[a-zA-Z]+$" {
+		t.Fatal("Expected first item in the array to be route A")
+	}
+	if mux.routes[0].hitCount != 100 {
+		t.Fatal("Expected route /A* to have a hit count of 100", mux.routes[0].hitCount)
+	}
+	if mux.routes[1].hitCount != 200 {
+		t.Fatal("Expected route /B* to have a hit count of 200 ", mux.routes[1].hitCount)
+	}
+
+	RouteOptimizeRequestCount = 250
+	for i:=0; i<100; i++ {
+		mux.ServeHTTP(response, requestB)
+	}
+
+	if mux.routes[0].pattern != "^/B[a-zA-Z]+$" {
+		t.Fatal("Expected first item in the array to be route B")
+	}
+	if mux.routes[0].hitCount != 300 {
+		t.Fatal("Expected route /A* to have a hit count of 300", mux.routes[0].hitCount)
+	}
+	if mux.routes[1].hitCount != 100 {
+		t.Fatal("Expected route /B* to have a hit count of 100 ", mux.routes[1].hitCount)
+	}
+
 }
